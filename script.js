a9c963bb3f15e18325e8782993321497243d298f
mjuilk <jonconneryl@gmail.com>
2021-07-24 16:59:28 +0200

    why dis shit not work?!

--- script.js
+++ script.js
@@ -5,27 +5,33 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let lowerPlayer = playerSelection.toLowerCase();
+    let result = document.querySelector('#result');
+
     if (lowerPlayer === computerSelection) {
-        return "DRAW!";
+        result.textContent = "DRAW!";
     }
     else if ((lowerPlayer === 'rock') && (computerSelection === 'scissors')) {
-        return "YOU WIN! Rock beats scissors!";
+        result.textContent =  "YOU WIN! Rock beats scissors!";
     }
     else if ((lowerPlayer === 'rock') && (computerSelection === 'paper')) {
-        return "YOU LOSE! Paper beats rock!";
+        result.textContent =  "YOU LOSE! Paper beats rock!";
     }
     else if ((lowerPlayer === 'scissors') && (computerSelection === 'paper')) {
-        return "YOU WIN! Scissors beats paper!";
+        result.textContent =  "YOU WIN! Scissors beats paper!";
     }
     else if ((lowerPlayer === 'scissors') && (computerSelection === 'rock')) {
-        return "YOU LOSE! Rock beats scissors!";
+        result.textContent =  "YOU LOSE! Rock beats scissors!";
     }
     else if ((lowerPlayer === 'paper') && (computerSelection === 'rock')) {
-        return "YOU WIN! Paper beats rock!";
+        result.textContent =  "YOU WIN! Paper beats rock!";
     }
     else if ((lowerPlayer === 'paper') && (computerSelection === 'scissors')) {
-        return "YOU LOSE! Scissors beats paper!";
+        result.textContent =  "YOU LOSE! Scissors beats paper!";
     }
 }
 
+let paper = document.getElementById("paper");
+document.getElementById("rock").addEventListener("click", playRound("rock", computerPlay()));
+document.getElementById('paper').onclick = function test() {
+    document.getElementById('paper').style = "Color: red";
+}
